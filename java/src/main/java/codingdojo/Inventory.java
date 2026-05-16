4aa5167801e8fdcb46b9f023f199eceb595b6cdb
fred <fredsedcoder@gmail.com>
2026-05-17 00:00:56 +0200

    move getDamageModifier to Inventory Class

--- java/src/main/java/codingdojo/Inventory.java
+++ java/src/main/java/codingdojo/Inventory.java
@@ -15,4 +15,18 @@ public class Inventory {
         return equipment.getBaseDamage();
     }
 
+    public float getDamageModifier(Stats stats) {
+        Item leftHand = equipment.getLeftHand();
+        Item rightHand = equipment.getRightHand();
+        Item head = equipment.getHead();
+        Item feet = equipment.getFeet();
+        Item chest = equipment.getChest();
+        float strengthModifier = stats.getStrength() * 0.1f;
+        return strengthModifier +
+            leftHand.getDamageModifier() +
+            rightHand.getDamageModifier() +
+            head.getDamageModifier() +
+            feet.getDamageModifier() +
+            chest.getDamageModifier();
+    }
 }
