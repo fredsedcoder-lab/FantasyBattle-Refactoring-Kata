24629abbface6f25ecfa3ea04130d222a355ff48
fred <fredsedcoder@gmail.com>
2026-05-17 00:30:32 +0200

    move soak calculation to simple enemy

--- java/src/main/java/codingdojo/Player.java
+++ java/src/main/java/codingdojo/Player.java
@@ -28,18 +28,8 @@ public class Player extends Target {
             // TODO: Not implemented yet
             //  Add friendly fire
             soak = totalDamage;
-        } else if (other instanceof SimpleEnemy) {
-            SimpleEnemy simpleEnemy = (SimpleEnemy) other;
-            soak = Math.round(
-                simpleEnemy.getArmor().getDamageSoak() *
-                (
-                    ((float) simpleEnemy.getBuffs()
-                        .stream()
-                        .mapToDouble(Buff::soakModifier)
-                        .sum()) +
-                    1f
-                )
-            );
+        } else if (other instanceof SimpleEnemy simpleEnemy) {
+            soak = simpleEnemy.getSimpleSoak();
         }
         return soak;
     }
